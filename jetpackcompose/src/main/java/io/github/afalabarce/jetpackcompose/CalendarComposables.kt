093a2bd02b4d2095eb9e7f320a969f4cd02b6f9c
afalabarce <afalabarce@gmail.com>
2022-05-22 15:30:21 +0200

    clean imports

--- jetpackcompose/src/main/java/io/github/afalabarce/jetpackcompose/CalendarComposables.kt
+++ jetpackcompose/src/main/java/io/github/afalabarce/jetpackcompose/CalendarComposables.kt
@@ -15,7 +15,6 @@ import androidx.compose.material.icons.rounded.ArrowForward
 import androidx.compose.material.icons.rounded.DateRange
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
