1c711fd435aa1f027a3369e367a8c0561d481e3c
afalabarce <afalabarce@gmail.com>
2022-06-21 15:35:01 +0200

    Agregada sobrecarga a SwipeableCard y agregada nueva propiedad unSwipeOnClick.

--- jetpackcompose/src/main/java/io/github/afalabarce/jetpackcompose/SwipeableCard.kt
+++ jetpackcompose/src/main/java/io/github/afalabarce/jetpackcompose/SwipeableCard.kt
@@ -8,12 +8,15 @@ import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.currentRecomposeScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.*
+import kotlinx.coroutines.*
 import kotlin.math.roundToInt
 
 data class SwipeAction(val order: Int, val key: String, val title: String, val imageVector: ImageVector, val color: Color, val tint: Color, val dockLeft: Boolean)
@@ -74,9 +77,56 @@ fun SwipeableCard(modifier: Modifier = Modifier,
                   shape: Shape = MaterialTheme.shapes.medium,
                   backgroundColor: Color = MaterialTheme.colors.surface,
                   buttonWidth: Dp,
+                  swipeBackColor: Color = Color.Transparent,
+                  contentColor: Color = contentColorFor(backgroundColor),
+                  border: BorderStroke? = null,
+                  elevation: Dp = 1.dp,
+                  swipeActions: Array<SwipeAction> = arrayOf(),
+                  onClickSwipeAction: (SwipeAction) -> Unit = { },
+                  unSwipeOnClick: Boolean = true,
+                  content: @Composable () -> Unit) = SwipeableCard(
+                                                            modifier = modifier,
+                                                            shape = shape,
+                                                            backgroundColor = backgroundColor,
+                                                            buttonWidth = buttonWidth,
+                                                            swipeBackColor = swipeBackColor,
+                                                            contentColor = contentColor,
+                                                            border = border,
+                                                            anchors = getAnchorMap(LocalDensity.current, buttonWidth, swipeActions),
+                                                            elevation = elevation,
+                                                            swipeActions = swipeActions,
+                                                            onClickSwipeAction = onClickSwipeAction,
+                                                            unSwipeOnClick = unSwipeOnClick,
+                                                            content = content
+                                                        )
+
+/**
+ * Swipeable Horizontal Card with custom actions
+ * @param modifier Modifier to be applied to the layout of the card.
+ * @param shape Defines the card's shape as well its shadow. A shadow is only
+ *  displayed if the [elevation] is greater than zero.
+ * @param backgroundColor The background color.
+ * @param contentColor The preferred content color provided by this card to its children.
+ * Defaults to either the matching content color for [backgroundColor], or if [backgroundColor]
+ * is not a color from the theme, this will keep the same value set above this card.
+ * @param border Optional border to draw on top of the card
+ * @param elevation The z-coordinate at which to place this card. This controls
+ *  the size of the shadow below the card.
+ *  @param buttonWidth SwipeActions fixed Width
+ *  @param swipeActions SwipeActions Array with left/right docking
+ *  @param onClickSwipeAction Raised event on SwipeAction tap
+ *  @param swipeBackColor Backcolor of swipe area
+ *  @param content Card Content
+ */
+@Composable
+fun SwipeableCard(modifier: Modifier = Modifier,
+                  shape: Shape = MaterialTheme.shapes.medium,
+                  backgroundColor: Color = MaterialTheme.colors.surface,
+                  buttonWidth: Dp,
                   anchors: Map<Float,Int> = mapOf(0f to 0),
                   swipeBackColor: Color = Color.Transparent,
                   contentColor: Color = contentColorFor(backgroundColor),
+                  unSwipeOnClick: Boolean = true,
                   border: BorderStroke? = null,
                   elevation: Dp = 1.dp,
                   swipeActions: Array<SwipeAction> = arrayOf(),
@@ -101,7 +151,16 @@ fun SwipeableCard(modifier: Modifier = Modifier,
                         swipeActions.filter { x -> x.dockLeft }.sortedBy { o -> o.order }
                             .map { action ->
                                 Button(
-                                    onClick = { onClickSwipeAction(action) },
+                                    onClick = {
+                                        onClickSwipeAction(action)
+                                        if (unSwipeOnClick){
+                                            GlobalScope.launch {
+                                                withContext(Dispatchers.Main){
+                                                    swipeableState.animateTo(0)
+                                                }
+                                            }
+                                        }
+                                    },
                                     modifier = Modifier
                                         .fillMaxHeight()
                                         .width(buttonWidth),
@@ -137,7 +196,16 @@ fun SwipeableCard(modifier: Modifier = Modifier,
                         swipeActions.filter { x -> !x.dockLeft }.sortedBy { o -> o.order }
                             .map { action ->
                                 Button(
-                                    onClick = { onClickSwipeAction(action) },
+                                    onClick = {
+                                        onClickSwipeAction(action)
+                                        if (unSwipeOnClick){
+                                            GlobalScope.launch {
+                                                withContext(Dispatchers.Main){
+                                                    swipeableState.animateTo(0)
+                                                }
+                                            }
+                                        }
+                                    },
                                     modifier = Modifier
                                         .fillMaxHeight()
                                         .width(buttonWidth),
