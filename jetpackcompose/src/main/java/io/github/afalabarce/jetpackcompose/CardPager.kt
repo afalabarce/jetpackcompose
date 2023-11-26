d9fee93054cbf298eb99c85b88ee1067ce280454
Antonio Fernández Alabarce <afalabarce@gmail.com>
2023-11-26 20:39:04 +0100

    Updated readme and build.gradle version and a little bugfix in SpinnerSelector

--- jetpackcompose/src/main/java/io/github/afalabarce/jetpackcompose/CardPager.kt
+++ jetpackcompose/src/main/java/io/github/afalabarce/jetpackcompose/CardPager.kt
@@ -1,9 +1,23 @@
 package io.github.afalabarce.jetpackcompose
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.contentColorFor
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -11,12 +25,11 @@ import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.HorizontalPagerIndicator
 import com.google.accompanist.pager.rememberPagerState
 
-@OptIn(ExperimentalPagerApi::class)
+
 @Composable
 fun CardPager(
     modifier: Modifier,
--- jetpackcompose/src/main/java/io/github/afalabarce/jetpackcompose/SpinnerSelector.kt
+++ jetpackcompose/src/main/java/io/github/afalabarce/jetpackcompose/SpinnerSelector.kt
@@ -1,13 +1,35 @@
 package io.github.afalabarce.jetpackcompose
 
-import android.media.Image
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowDropDown
 import androidx.compose.material.icons.rounded.ArrowDropUp
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -15,9 +37,7 @@ import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.PopupProperties
 import androidx.constraintlayout.compose.ConstraintLayout
-import io.github.afalabarce.jetpackcompose.utilities.iif
 
 @Composable
 fun <T> SpinnerSelector(
@@ -38,14 +58,14 @@ fun <T> SpinnerSelector(
     onBackgroundColor: Color = MaterialTheme.colorScheme.background,
     items: List<T>,
     onSelectedItem: (T) -> Unit = {},
-    itemComposable: @Composable (T) -> Unit = { i -> Text(i.toString())}
+    itemComposable: @Composable (T) -> Unit = { i -> Text(i.toString()) }
 ) {
     var expandedDropDown by remember { mutableStateOf(false) }
     var spinnerValue: T? by remember { mutableStateOf(null) }
 
     if (selectedItem != null)
         spinnerValue = selectedItem
-    Box(modifier = modifier.height((spinnerValue != null).iif(componentHeight.plus(6.dp), componentHeight))){
+    Box(modifier = modifier.height(componentHeight.plus(12.dp))){
         Button(
             onClick = {
                 if (!readOnly)
@@ -53,6 +73,7 @@ fun <T> SpinnerSelector(
             },
             modifier = modifier
                 .height(componentHeight)
+                .offset(y = 6.dp)
                 .align(Alignment.BottomStart),
             shape = shape,
             colors = colors,
@@ -75,10 +96,10 @@ fun <T> SpinnerSelector(
                         bottom.linkTo(parent.bottom)
                         end.linkTo(parent.end)
                     }.focusable(false)
-                    .clickable(true) {
-                        if (!readOnly)
-                            expandedDropDown = !expandedDropDown
-                    }
+                        .clickable(true) {
+                            if (!readOnly)
+                                expandedDropDown = !expandedDropDown
+                        }
                 )
 
                 Column(
@@ -127,7 +148,7 @@ fun <T> SpinnerSelector(
         if (spinnerValue != null && hintText.isNotEmpty()){
             Column(
                 modifier = modifier
-                    .height((spinnerValue != null).iif(componentHeight.plus(6.dp), componentHeight))
+                    .height(componentHeight.plus(12.dp))
                     .padding(start = 16.dp)
                     .background(Color.Transparent),
                 verticalArrangement = Arrangement.Top
